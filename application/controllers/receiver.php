4eb8d9cdc642fd3374252bb424ecbb46c8e1f166
Ryan Chan <rcchan@cs.brown.edu>
2012-04-17 20:59:32 -0400

    TwiML backend

---
+++ application/controllers/receiver.php
@@ -0,0 +1,31 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Receiver extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+    $this->load->library('twilio');
+    $this->load->library('tank_auth');
+	}
+
+	function index()
+	{
+		header('Status: 403 FORBIDDEN');
+	}
+
+  function sms(){
+    if ($_SERVER['REQUEST_METHOD'] != 'GET' || !$_GET['Body'] || !$_GET['From'] || !$_GET['To']){
+      $this->load->view('twiml.php', array('message' => 'Invalid request!'));
+      return;
+    }
+    switch(substr($_GET['Body'], 0, 2)){
+      case 'st':
+        $this->load->view('twiml.php', array('redirect' => '/stream/post'));
+        break;
+      default:
+        $this->load->view('twiml.php', array('message' => 'Invalid module code'));
+    }
+  }
+}
+?>
