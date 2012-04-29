a4629c18f78860ad18d120456d0657f5943665aa
Ryan Chan <rcchan@cs.brown.edu>
2012-04-29 02:40:45 -0400

    Dashboard populate classes

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -8,7 +8,7 @@ class Welcome extends CI_Controller {
     $this->load->helper('form');
     $this->load->helper('url');
   }
-  function _checkauth(){
+  function _checkauth($data = array()){
     if ($this->tank_auth->is_logged_in()){
       $data['user_id'] = $this->tank_auth->get_user_id();
       $data['username'] = $this->tank_auth->get_username();
@@ -23,7 +23,7 @@ class Welcome extends CI_Controller {
     $this->load->view('modules/' . $view, $data);
   }
   function render($view, $data = array()){
-    $data = $this->_checkauth();
+    $data = $this->_checkauth($data);
     $data['page'] = $view;
     $this->load->view('header', $data);
     $this->load->view($view, $data);
@@ -33,7 +33,16 @@ class Welcome extends CI_Controller {
     if ($this->tank_auth->is_logged_in()) $this->render($view, $data);
     else redirect($redirect);
   }
+  
   function index(){
     $this->render('home');
+  }
+  function dashboard(){
+    $data = $this->_checkauth($data);
+    if ($data['user_id']){
+      $data['classlist'] = $this->db->query('SELECT id, name FROM classlist WHERE owner_id = ?', array($data['user_id']))->result_array();
+      $this->render_secure('dashboard', $data);
+    }
+    else redirect($redirect);
   }
 }
