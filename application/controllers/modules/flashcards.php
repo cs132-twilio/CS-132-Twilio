9a878582fca8a985c3447f6645bf2eb206d2ffaf
pciullo <peter_ciullo@gmail.com>
2012-04-30 19:05:37 -0400

    message count

--- application/controllers/modules/flashcards.php
+++ application/controllers/modules/flashcards.php
@@ -19,7 +19,8 @@ class FlashCards extends CI_Controller
       $this->load->view('twiml.php', array('message' => 'Invalid request!'));
       return;
     }
-    $message = preg_split('/[\s]+/', $_GET['Body'], 3);    
+    $message = preg_split('/[\s]+/', $_GET['Body'], 3); 
+    $this->load->view('twiml.php', array('message' => count($message)));
     if (count($message) < 1){
       $this->load->view('twiml.php', array('message' => 'Invalid request!'));
       return;
