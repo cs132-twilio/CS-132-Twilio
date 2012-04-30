7b9c06604c3674ab6717718ce915c5ff1e124f41
Angela Santin Ceballos <asantinc@cs.brown.edu>
2012-04-29 21:41:00 -0400

    Added the profile page

---
+++ application/views/profile.php
@@ -0,0 +1,11 @@
+<div class ="row-fluid">
+<form class="well">
+  <label>Display name</label>
+  <input type="text" class="span3" placeholder="Type something…">
+  <span class="help-block">Example block-level help text here.</span>
+  <label class="checkbox">
+    <input type="checkbox"> Check me out
+  </label>
+  <button type="submit" class="btn">Submit</button>
+</form>
+</div>
