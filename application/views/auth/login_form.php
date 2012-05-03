10cb23cb28c2169c6c8d93b500ec5bf9900556c3
Ryan Chan <rcchan@cs.brown.edu>
2012-05-03 17:01:08 -0400

    Login redirect

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -34,7 +34,7 @@ $captcha = array(
 <script type="text/javascript">
   if (window.location.href.match(/auth\/login$/)) window.location.href='/';
 </script>
-<?php echo form_open($this->uri->uri_string()); ?>
+<?php echo form_open($this->uri->uri_string() . '?' . htmlentities($_SERVER['QUERY_STRING'])); ?>
 <table>
 	<tr>
 		<td><?php echo form_label($login_label, $login['id']); ?></td>
