ab060f68a1db2e9d046f6288e853f38beb3b12e0
asantinc <angiedaniela@gmail.com>
2012-05-05 16:14:54 -0400

    Got rid of id and number displayed on the list. We should not give students access to their numbers for security reasons.

--- application/views/modules/list.php
+++ application/views/modules/list.php
@@ -1,18 +1,17 @@
 <link rel="Stylesheet" type="text/css" href="/assets/stylesheets/modules/list.css">
 <script type="text/javascript" src="/assets/javascript/List.js"></script>
-Class ID: <?= $class_id ?>
+
 
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>Name</th>
-      <th>Number</th>
+      <th>Student Name</th>
     </tr>
   </thead>
   <tbody>
     <?
     foreach($students as $s){
-      echo '<tr data-sid="' . $s['id'] . '"><td>' . htmlentities($s['name']) . '</td><td>' . htmlentities($s['number']) . '</td><td><img class="list_removestudent" src="/assets/images/glyphicons_207_remove_2.png"</td></tr>';
+      echo '<tr data-sid="' . $s['id'] . '"><td>' . htmlentities($s['name']) . '</td><td><img class="list_removestudent" src="/assets/images/glyphicons_207_remove_2.png"</td></tr>';
     }
     ?>
   </tbody>
