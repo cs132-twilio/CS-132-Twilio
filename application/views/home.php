a0b8b3b931a509f607512270d800e5d88ca54d05
pciullo <peter_ciullo@gmail.com>
2012-05-06 17:24:37 -0400

    homepage logged in space11

--- application/views/home.php
+++ application/views/home.php
@@ -1,39 +1,39 @@
 <script type="text/javascript"  src="/assets/javascript/home.js"></script>
 <div class="hero-unit span8 offset1 welcome_container">
   <div class="row">
+   <h1 class="span8">Twexter</h1>
+  </div>
+  <div class="row">       
     <div class="span5">
-      <h1 class="span5">Twexter</h1>
-      <div class="span5">
-        <div id="myCarousel" class="carousel slide">
-          <div class="carousel-inner">
-            <div class="item active">
-              <img src="/assets/images/phone.jpg" alt="">
-              <div class="carousel-caption">
-                <h4>Rediscover SMS</h4>
-                <p>All of your students can text. We help you incorporate this into the class.</p>
-              </div>
-            </div>
-            <div class="item">
-              <img src="/assets/images/students.jpg" alt="">
-              <div class="carousel-caption">
-                <h4>Student engagement</h4>
-                <p>We provide you a simple way to keep your students engaged. They will be more involved, and you will have more time to teach.</p>
-              </div>
-            </div>
-            <div class="item">
-              <img src="/assets/images/graduate.jpg" alt="">
-              <div class="carousel-caption">
-                <h4>Study aid</h4>
-                <p>Texting can now help you connect with your students, and help them achieve their goals.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div id="myCarousel" class="carousel slide">
+	<div class="carousel-inner">
+	  <div class="item active">
+	    <img src="/assets/images/phone.jpg" alt="">
+	    <div class="carousel-caption">
+	      <h4>Rediscover SMS</h4>
+	      <p>All of your students can text. We help you incorporate this into the class.</p>
+	    </div>
+	  </div>
+	  <div class="item">
+	    <img src="/assets/images/students.jpg" alt="">
+	    <div class="carousel-caption">
+	      <h4>Student engagement</h4>
+	      <p>We provide you a simple way to keep your students engaged. They will be more involved, and you will have more time to teach.</p>
+	    </div>
+	  </div>
+	  <div class="item">
+	    <img src="/assets/images/graduate.jpg" alt="">
+	    <div class="carousel-caption">
+	      <h4>Study aid</h4>
+	      <p>Texting can now help you connect with your students, and help them achieve their goals.</p>
+	    </div>
+	  </div>
+	</div>
+      </div>    
     </div>
     <div class="span3">      
       <?= $user_id?'<div id="logged-in-home" class="span3">
-	<h2 style="margin-top:30px; margin-bottom:30px;">You\'re logged in to twexter. Head over to the dashboard to manage your classes.</h2>
+	<h2 style="margin-top:40px; margin-bottom:50px;">You\'re logged in to twexter. Head over to the dashboard to manage your classes.</h2>
 	<p>
 	<a href="dashboard" class="btn btn-large btn-info big-button ">Go to Dashboard</a> 
 	</p>
