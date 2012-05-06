b4d828d68420d3dfa1201b91ed2ced2c0e6df39e
pciullo <peter_ciullo@gmail.com>
2012-05-06 19:05:37 -0400

    removed meta tags

--- application/views/header.php
+++ application/views/header.php
@@ -6,10 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"> 
-    <meta http-equiv="Pragma" content="no-cache"> 
-    <meta http-equiv="Expires" content="-1"> 
 
     <link href="/assets/stylesheets/bootstrap.css" rel="stylesheet">
     <link href="/assets/stylesheets/bootstrap-responsive.css" rel="stylesheet">
