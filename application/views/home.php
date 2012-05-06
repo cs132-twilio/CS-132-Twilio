ccb9684f08fd8f4ad14808f34a4d9656f4826bc4
pciullo <peter_ciullo@gmail.com>
2012-05-06 17:18:03 -0400

    homepage logged in space6

--- application/views/home.php
+++ application/views/home.php
@@ -33,9 +33,9 @@
     </div>
     <div class="span3">
       <h1 class="span3">Try Now</h1>
-      <?= $user_id?'<div id="logged-in-home">
-	<h2>You\'re logged in to twexter. Head over to the dashboard to manage your classes.</h2>
-	<a href="dashboard" class="btn btn-large btn-info big-button">Go to Dashboard</a>
+      <?= $user_id?'<div id="logged-in-home" class="span3">
+	<h2 class="span3">You\'re logged in to twexter. Head over to the dashboard to manage your classes.</h2>
+	<a href="dashboard" class="btn btn-large btn-info big-button span3">Go to Dashboard»</a>
       
       
       </div>':'<div class="span3" id="registration"></div>' ?>
