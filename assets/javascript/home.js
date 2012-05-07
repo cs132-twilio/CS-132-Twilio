2ca06bfec9e94cc379e1c4e499b392fb44da5c57
Ryan Chan <rcchan@cs.brown.edu>
2012-05-06 20:37:43 -0400

    Registration fix

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -2,14 +2,27 @@
   $(window).load(
     function(){
       var callback = function(){
+        $('#registration > form button[name=next]').click(
+          function(){
+            $('#registration-page-1').animate({'margin-left':'-100%'}, function(){$('#recaptcha_response_field').focus();});
+          }
+        );
+        $('#registration > form button[name=back]').click(
+          function(){
+            $('#registration-page-1').animate({'margin-left':'0%'});
+          }
+        );
         $('#registration > form').submit(
           function(){
-            !$(this).ajaxSubmit(
-              function (r){
-                $('#registration').html(r);
-                callback();
-              }
-            );
+            if ($('#registration-page-1').css('margin-left') != '-100%') $('#registration > form button[name=next]').click();
+            else {
+              $(this).ajaxSubmit(
+                function (r){
+                  $('#registration').html(r=='You have successfully registered. Check your email address to activate your account.'?'<p /><p>You have successfully registered.<br/>Check your email address to<br />activateyour account.</p>':r);
+                  callback();
+                }
+              );
+            }
             return false;
           }
         );
